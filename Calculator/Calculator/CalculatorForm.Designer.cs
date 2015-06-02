ae3eec6290997aac800967fb87ee842f25f3303d
Tom <redsoxfantom@gmail.com>
2015-06-02 19:26:18 -0400

    Renamed Calculator Form

--- Calculator/Calculator/CalculatorForm.Designer.cs
+++ Calculator/Calculator/CalculatorForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Calculator
 {
-    partial class Form1
+    partial class CalculatorForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(674, 526);
+            this.Name = "Form1";
+            this.Text = "CSC 8600 Calculator";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
