d9c8c36bbf531c5edbc58c60ddd10da4f1476ccc
Tom <redsoxfantom@gmail.com>
2015-06-02 19:40:13 -0400

    Added function keys to calculator

--- Calculator/Calculator/CalculatorForm.Designer.cs
+++ Calculator/Calculator/CalculatorForm.Designer.cs
@@ -39,13 +39,18 @@
             this.Eight = new System.Windows.Forms.Button();
             this.Seven = new System.Windows.Forms.Button();
             this.Zero = new System.Windows.Forms.Button();
+            this.Plus = new System.Windows.Forms.Button();
+            this.Minus = new System.Windows.Forms.Button();
+            this.Multiply = new System.Windows.Forms.Button();
+            this.Divide = new System.Windows.Forms.Button();
+            this.Equals = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(13, 13);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(649, 20);
+            this.textBox1.Size = new System.Drawing.Size(206, 20);
             this.textBox1.TabIndex = 0;
             // 
             // One
@@ -140,7 +145,7 @@
             // 
             // Zero
             // 
-            this.Zero.Location = new System.Drawing.Point(66, 181);
+            this.Zero.Location = new System.Drawing.Point(38, 181);
             this.Zero.Name = "Zero";
             this.Zero.Size = new System.Drawing.Size(47, 41);
             this.Zero.TabIndex = 10;
@@ -148,11 +153,66 @@
             this.Zero.Text = "0";
             this.Zero.UseVisualStyleBackColor = true;
             // 
+            // Plus
+            // 
+            this.Plus.Location = new System.Drawing.Point(172, 40);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(47, 41);
+            this.Plus.TabIndex = 11;
+            this.Plus.Tag = "+";
+            this.Plus.Text = "+";
+            this.Plus.UseVisualStyleBackColor = true;
+            // 
+            // Minus
+            // 
+            this.Minus.Location = new System.Drawing.Point(172, 87);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(47, 41);
+            this.Minus.TabIndex = 12;
+            this.Minus.Tag = "-";
+            this.Minus.Text = "-";
+            this.Minus.UseVisualStyleBackColor = true;
+            // 
+            // Multiply
+            // 
+            this.Multiply.Location = new System.Drawing.Point(172, 134);
+            this.Multiply.Name = "Multiply";
+            this.Multiply.Size = new System.Drawing.Size(47, 41);
+            this.Multiply.TabIndex = 13;
+            this.Multiply.Tag = "*";
+            this.Multiply.Text = "*";
+            this.Multiply.UseVisualStyleBackColor = true;
+            // 
+            // Divide
+            // 
+            this.Divide.Location = new System.Drawing.Point(172, 181);
+            this.Divide.Name = "Divide";
+            this.Divide.Size = new System.Drawing.Size(47, 41);
+            this.Divide.TabIndex = 14;
+            this.Divide.Tag = "/";
+            this.Divide.Text = "/";
+            this.Divide.UseVisualStyleBackColor = true;
+            // 
+            // Equals
+            // 
+            this.Equals.Location = new System.Drawing.Point(91, 181);
+            this.Equals.Name = "Equals";
+            this.Equals.Size = new System.Drawing.Size(47, 41);
+            this.Equals.TabIndex = 15;
+            this.Equals.Tag = "=";
+            this.Equals.Text = "=";
+            this.Equals.UseVisualStyleBackColor = true;
+            // 
             // CalculatorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(674, 526);
+            this.ClientSize = new System.Drawing.Size(234, 233);
+            this.Controls.Add(this.Equals);
+            this.Controls.Add(this.Divide);
+            this.Controls.Add(this.Multiply);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Plus);
             this.Controls.Add(this.Zero);
             this.Controls.Add(this.Nine);
             this.Controls.Add(this.Eight);
@@ -184,6 +244,11 @@
         private System.Windows.Forms.Button Eight;
         private System.Windows.Forms.Button Seven;
         private System.Windows.Forms.Button Zero;
+        private System.Windows.Forms.Button Plus;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button Multiply;
+        private System.Windows.Forms.Button Divide;
+        private System.Windows.Forms.Button Equals;
     }
 }
 
