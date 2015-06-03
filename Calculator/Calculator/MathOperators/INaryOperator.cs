81de10800bd3b8fd2c5ae4aa83470017e0a855cf
Tom <redsoxfantom@gmail.com>
2015-06-02 21:13:11 -0400

    Creates N-ary operator interface

---
+++ Calculator/Calculator/MathOperators/INaryOperator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.BinaryOperators
+{
+    /// <summary>
+    /// Interface for all operators that take many operands
+    /// </summary>
+    public interface INaryOperator
+    {
+        /// <summary>
+        /// Takes a list of operands and performs some operation on them
+        /// </summary>
+        /// <param name="operands">A list of operands</param>
+        /// <returns>The result of the operation</returns>
+        public double PerformOperation(params double[] operands);
+    }
+}
