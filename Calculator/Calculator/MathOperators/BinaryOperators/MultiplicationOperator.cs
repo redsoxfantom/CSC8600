f943baf2cd4b6a3e3291ec306e6d74f2a7142b2c
Tom <redsoxfantom@gmail.com>
2015-06-02 21:31:25 -0400

    Created Multiplication Operator

---
+++ Calculator/Calculator/MathOperators/BinaryOperators/MultiplicationOperator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MathOperators.BinaryOperators
+{
+    /// <summary>
+    /// Multiplies two numbers together
+    /// </summary>
+    public class MultiplicationOperator : BinaryOperator
+    {
+        /// <summary>
+        /// Multiply two numbers
+        /// </summary>
+        /// <param name="operand1">The first number</param>
+        /// <param name="operand2">The second number</param>
+        /// <returns>The result</returns>
+        protected override double PerformOperation(double operand1, double operand2)
+        {
+            double result = operand1 * operand2;
+
+            return result;
+        }
+    }
+}
