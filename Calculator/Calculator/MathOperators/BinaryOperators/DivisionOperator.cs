8f445cb8b68e9fe2c21b519c8dc82f077a6b3e74
Tom <redsoxfantom@gmail.com>
2015-06-02 21:30:08 -0400

    Created Division Operator

---
+++ Calculator/Calculator/MathOperators/BinaryOperators/DivisionOperator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MathOperators.BinaryOperators
+{
+    /// <summary>
+    /// Divide two numbers
+    /// </summary>
+    public class DivisionOperator : BinaryOperator
+    {
+        /// <summary>
+        /// Divide two numbers, return the result
+        /// </summary>
+        /// <param name="operand1">The first number</param>
+        /// <param name="operand2">The second number</param>
+        /// <returns>The result</returns>
+        protected override double PerformOperation(double operand1, double operand2)
+        {
+            double result = operand1 / operand2;
+
+            return result;
+        }
+    }
+}
