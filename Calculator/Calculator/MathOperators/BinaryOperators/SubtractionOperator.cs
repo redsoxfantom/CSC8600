3b9863d3e41cfa9753572bcd0baa8c0ea6795e86
Tom <redsoxfantom@gmail.com>
2015-06-02 21:28:49 -0400

    Created Subtraction Operator

---
+++ Calculator/Calculator/MathOperators/BinaryOperators/SubtractionOperator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MathOperators.BinaryOperators
+{
+    /// <summary>
+    /// Subtracts two numbers
+    /// </summary>
+    public class SubtractionOperator : BinaryOperator
+    {
+        /// <summary>
+        /// Subtract two numbers
+        /// </summary>
+        /// <param name="operand1">The first number</param>
+        /// <param name="operand2">The second number</param>
+        /// <returns>The result</returns>
+        protected override double PerformOperation(double operand1, double operand2)
+        {
+            double result = operand1 - operand2;
+
+            return result;
+        }
+    }
+}
