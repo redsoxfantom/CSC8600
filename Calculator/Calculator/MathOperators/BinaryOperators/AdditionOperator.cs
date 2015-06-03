5914a94bb6b15882ddcbddd55b278b0f8f5fb8d8
Tom <redsoxfantom@gmail.com>
2015-06-02 21:19:26 -0400

    Created AdditionOperator and MathOperatorException

---
+++ Calculator/Calculator/MathOperators/BinaryOperators/AdditionOperator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MathOperators.BinaryOperators
+{
+    /// <summary>
+    /// Binary operator that adds two operands together
+    /// </summary>
+    public class AdditionOperator : INaryOperator
+    {
+        /// <summary>
+        /// Adds two numbers together
+        /// </summary>
+        /// <param name="operands">A list of operands. Must equal 2</param>
+        /// <returns>The two operands added together</returns>
+        public double PerformOperation(params double[] operands)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// This operator requires two operands
+        /// </summary>
+        /// <returns>2</returns>
+        public int NumOperandsExpected()
+        {
+            return 2;
+        }
+    }
+}
---
+++ Calculator/Calculator/MathOperators/MathOperatorException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MathOperators
+{
+    /// <summary>
+    /// Exception thrown by all math operators
+    /// </summary>
+    public class MathOperatorException : Exception
+    {
+    }
+}
