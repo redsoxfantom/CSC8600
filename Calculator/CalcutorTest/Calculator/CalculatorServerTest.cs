a9c3550a929e35f381d60c4d9e115407ec0def7a
Tom <redsoxfantom@gmail.com>
2015-06-04 18:37:34 -0400

    Initial creation of CalculatorServerTest

---
+++ Calculator/CalcutorTest/Calculator/CalculatorServerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.Calculator;
+
+namespace CalcutorTest.Calculator
+{
+    /// <summary>
+    /// Test of the Calculator class
+    /// </summary>
+    [TestClass]
+    public class CalculatorServerTest
+    {
+        private CalculatorServer target;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
