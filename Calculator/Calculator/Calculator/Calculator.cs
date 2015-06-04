a1c872dbd921a74f0b7b8549cf504e5011ea276c
Tom <redsoxfantom@gmail.com>
2015-06-04 18:14:17 -0400

    Began work on master Calculator class

---
+++ Calculator/Calculator/Calculator/Calculator.cs
@@ -0,0 +1,52 @@
+﻿using Calculator.MathOperators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Implements the Calculator functionality
+    /// </summary>
+    public class Calculator
+    {
+        /// <summary>
+        /// The number the user entered
+        /// </summary>
+        private double mEnteredNumber;
+
+        /// <summary>
+        /// The constructor
+        /// </summary>
+        public Calculator()
+        {
+            mEnteredNumber = 0;
+        }
+
+        /// <summary>
+        /// Called when the user presses a number on the calculator GUI
+        /// </summary>
+        /// <param name="number">The number the user entered</param>
+        public void AcceptNumber(string number)
+        {
+            StringBuilder currentNumberString = new StringBuilder(mEnteredNumber.ToString());
+            currentNumberString.Append(number);
+
+            if(!Double.TryParse(currentNumberString.ToString(), out mEnteredNumber))
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Called when the user presses an operator key
+        /// </summary>
+        /// <param name="op">The operator</param>
+        public void AcceptOperator(INaryOperator op)
+        {
+
+        }
+    }
+}
