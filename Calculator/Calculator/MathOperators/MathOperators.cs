5c6a5b94f65807dee921c8c9b53967c21c505bc6
Tom <redsoxfantom@gmail.com>
2015-06-03 20:15:20 -0400

    Created math operators factory class

---
+++ Calculator/Calculator/MathOperators/MathOperators.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MathOperators
+{
+    /// <summary>
+    /// Factory class that returns a math operator given that operator's symbol.
+    /// </summary>
+    public class MathOperators
+    {
+        /// <summary>
+        /// Stores the list of operators and their symbols
+        /// </summary>
+        private static Dictionary<string, INaryOperator> operators = new Dictionary<string, INaryOperator>();
+
+        /// <summary>
+        /// Given an operator's symbol, return an instance of the INaryOperator represented by that symbol
+        /// </summary>
+        /// <param name="symbol">The symbol associated with an operator (ex: + => AdditionOperator)</param>
+        /// <returns>The N-Ary operator</returns>
+        public static INaryOperator GetOperator(string symbol)
+        {
+            if(operators.ContainsKey(symbol))
+            {
+                return operators[symbol];
+            }
+            else
+            {
+                throw new MathOperatorException(string.Format("Math symbol {0} not defined", symbol));
+            }
+        }
+    }
+}
