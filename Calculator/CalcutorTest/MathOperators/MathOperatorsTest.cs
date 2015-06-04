6ddcdd14e8066dba6759477130f5b1ccc1a6bd39
Tom <redsoxfantom@gmail.com>
2015-06-03 20:29:49 -0400

    Added test of the MathOperatorsFactory

---
+++ Calculator/CalcutorTest/MathOperators/MathOperatorsTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.MathOperators;
+using System.Collections.Generic;
+using System.Reflection;
+using Moq;
+
+namespace CalcutorTest.MathOperators
+{
+    /// <summary>
+    /// Test of the MathOperators factory class
+    /// </summary>
+    [TestClass]
+    public class MathOperatorsTest
+    {
+        /// <summary>
+        /// The target's operators
+        /// </summary>
+        private Dictionary<string, INaryOperator> operators;
+
+        /// <summary>
+        /// A mocked out operator
+        /// </summary>
+        private Mock<INaryOperator> opMock;
+
+        /// <summary>
+        /// Initialize the test
+        /// </summary>
+        [TestInitialize]
+        public void Initialize()
+        {
+            opMock = new Mock<INaryOperator>();
+            operators = new Dictionary<string, INaryOperator>();
+            FieldInfo operatorsField = typeof(MathOperatorsFactory).GetField("operators", BindingFlags.NonPublic | BindingFlags.Static);
+            operatorsField.SetValue(null, operators);
+        }
+
+        /// <summary>
+        /// Test the get operator method when the operator is defined
+        /// </summary>
+        [TestMethod]
+        public void OpFactoryGetExistingOperator()
+        {
+            operators.Add("+", opMock.Object);
+
+            INaryOperator actual = MathOperatorsFactory.GetOperator("+");
+
+            Assert.AreEqual(opMock.Object, actual);
+        }
+
+        /// <summary>
+        /// Test the getOperator method when the operator is not defined
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(MathOperatorException))]
+        public void OpFactoryGetNonExistantOperator()
+        {
+            INaryOperator op = MathOperatorsFactory.GetOperator("BAD_OP");
+        }
+    }
+}
