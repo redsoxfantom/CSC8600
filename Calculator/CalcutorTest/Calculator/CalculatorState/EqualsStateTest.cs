4109d0482861d15b01091e8410b9ee945766f254
Tom <redsoxfantom@gmail.com>
2015-06-06 17:52:19 -0400

    Created initialize method for equalsstatetest

--- Calculator/CalcutorTest/Calculator/CalculatorState/EqualsStateTest.cs
+++ Calculator/CalcutorTest/Calculator/CalculatorState/EqualsStateTest.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Calculator.Calculator.CalculatorState;
+using System.Collections.Generic;
+using Calculator.MathOperators;
+using Moq;
 
 namespace CalcutorTest.Calculator.CalculatorState
 {
@@ -20,5 +23,33 @@ namespace CalcutorTest.Calculator.CalculatorState
         /// </summary>
         private PrivateObject po;
 
+        /// <summary>
+        /// List of operators
+        /// </summary>
+        private List<INaryOperator> operatorsList;
+
+        /// <summary>
+        /// List of operands
+        /// </summary>
+        private List<double> operandsList;
+
+        /// <summary>
+        /// A mocked out operator
+        /// </summary>
+        private Mock<INaryOperator> op;
+
+        /// <summary>
+        /// Initialize the test
+        /// </summary>
+        [TestInitialize]
+        public void Init()
+        {
+            operandsList = new List<double>();
+            operatorsList = new List<INaryOperator>();
+            op = new Mock<INaryOperator>();
+
+            target = new EqualsState(operandsList, operatorsList);
+            po = new PrivateObject(target);
+        }
     }
 }
