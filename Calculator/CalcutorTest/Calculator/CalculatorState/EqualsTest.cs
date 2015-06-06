0fcd7dc4dbc50f3919ce92e7f9e40eaccd008779
Tom <redsoxfantom@gmail.com>
2015-06-06 17:46:48 -0400

    Initial development of the EqualsState class

---
+++ Calculator/CalcutorTest/Calculator/CalculatorState/EqualsTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcutorTest.Calculator.CalculatorState
+{
+    /// <summary>
+    /// Test of the Equals State class
+    /// </summary>
+    [TestClass]
+    public class EqualsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
