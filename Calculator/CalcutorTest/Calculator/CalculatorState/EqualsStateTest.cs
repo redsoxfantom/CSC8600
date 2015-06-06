d41346d5d65355c1159a03e95804e06e3ded0605
Tom <redsoxfantom@gmail.com>
2015-06-06 17:48:19 -0400

    Renamed EqualsTest to EqualsStateTest

---
+++ Calculator/CalcutorTest/Calculator/CalculatorState/EqualsStateTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.Calculator.CalculatorState;
+
+namespace CalcutorTest.Calculator.CalculatorState
+{
+    /// <summary>
+    /// Test of the Equals State class
+    /// </summary>
+    [TestClass]
+    public class EqualsStateTest
+    {
+        /// <summary>
+        /// The class under test
+        /// </summary>
+        private EqualsState target;
+
+        /// <summary>
+        /// Provides access to private methods and fields
+        /// </summary>
+        private PrivateObject po;
+
+    }
+}
