83622655cb97018d05f63c4078a88477d2c823e9
Tom <redsoxfantom@gmail.com>
2015-06-08 17:09:40 -0400

    Converted calculator server unit tests to use states

--- Calculator/CalcutorTest/Calculator/CalculatorServerTest.cs
+++ Calculator/CalcutorTest/Calculator/CalculatorServerTest.cs
@@ -5,6 +5,7 @@ using Moq;
 using Calculator.MathOperators.MathOperatorsFactory;
 using Calculator.MathOperators;
 using System.Collections.Generic;
+using Calculator.Calculator.CalculatorState;
 
 namespace CalcutorTest.Calculator
 {
@@ -30,15 +31,15 @@ namespace CalcutorTest.Calculator
         private Mock<IMathOperatorsFactory> mockFactory;
 
         /// <summary>
+        /// The mocked out state of the calculator
+        /// </summary>
+        private Mock<ICalculatorState> mockState;
+
+        /// <summary>
         /// A mocked out operator
         /// </summary>
         private Mock<INaryOperator> mockOp;
-
-        /// <summary>
-        /// The list of numbers entered by the user
-        /// </summary>
-        private List<double> previouslyEnteredNumbers;
-
+        
         /// <summary>
         /// Initialize the test
         /// </summary>
@@ -48,9 +49,10 @@ namespace CalcutorTest.Calculator
             target = new CalculatorServer();
             po = new PrivateObject(target);
             mockFactory = new Mock<IMathOperatorsFactory>();
+            mockState = new Mock<ICalculatorState>();
             mockOp = new Mock<INaryOperator>();
 
-            previouslyEnteredNumbers = (List<double>)po.GetFieldOrProperty("previouslyEnteredNumbers");
+            po.SetFieldOrProperty("mState", mockState.Object);
             po.SetFieldOrProperty("opFactory", mockFactory.Object);
             mockFactory.Setup(f => f.GetOperator("Good Op")).Returns(mockOp.Object);
             mockFactory.Setup(f => f.GetOperator("Bad Op")).Throws(new MathOperatorException());
@@ -68,75 +70,26 @@ namespace CalcutorTest.Calculator
         }
 
         /// <summary>
-        /// Test the Accept Number when given invalid input
+        /// Test the Accept Number method
         /// </summary>
         [TestMethod]
-        [ExpectedException(typeof(CalculatorException))]
-        public void CalculatorServerTestNumberBadChar()
-        {
-            target.AcceptNumber("a");
-        }
-
-        /// <summary>
-        /// Test the Accept Number method when given good input
-        /// </summary>
-        [TestMethod]
-        public void CalculatorServerTestNumberGoodNumber()
+        public void CalculatorServerTestNumber()
         {
             target.AcceptNumber("1");
 
-            double actual = (double)po.GetFieldOrProperty("mDisplayedNumber");
-            double expected = 1;
-
-            Assert.AreEqual(expected, actual);
+            mockState.Verify(f => f.OperandTransition("1"), Times.Once());
         }
 
         /// <summary>
-        /// Test the Accept Number method when given multiple good input
-        /// </summary>
-        [TestMethod]
-        public void CalculatorServerTestNumberMultipleGood()
-        {
-            target.AcceptNumber("1");
-            target.AcceptNumber("2");
-            target.AcceptNumber("3");
-
-            double actual = (double)po.GetFieldOrProperty("mDisplayedNumber");
-            double expected = 123;
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        /// <summary>
-        /// Test the Accept Number method when the resulting double would be more than the max possible
-        /// </summary>
-        [TestMethod]
-        [ExpectedException(typeof(CalculatorException))]
-        public void CalculatorServerTestNumberBadMax()
-        {
-            double num = double.MaxValue;
-            po.SetFieldOrProperty("mDisplayedNumber", num);
-
-            target.AcceptNumber("1");
-        }
-
-        /// <summary>
-        /// Test the AcceptOperator method when given good input
+        /// Test the AcceptOperator method
         /// </summary>
         [TestMethod]
         public void CalculatorServerTestOperatorGood()
         {
-            po.SetFieldOrProperty("mDisplayedNumber", 1234);
-
             target.AcceptOperator("Good Op");
 
             mockFactory.Verify(f => f.GetOperator("Good Op"), Times.Once());
-            INaryOperator actual = (INaryOperator)po.GetFieldOrProperty("currentOperator");
-            Assert.AreEqual(mockOp.Object, actual);
-            Assert.AreEqual(1, previouslyEnteredNumbers.Count);
-            Assert.AreEqual(1234, previouslyEnteredNumbers[0]);
-            double mEnteredNumber = (double)po.GetFieldOrProperty("mDisplayedNumber");
-            Assert.AreEqual(0, mEnteredNumber);
+            mockState.Verify(f => f.OperatorTransition(mockOp.Object), Times.Once());
         }
 
         /// <summary>
@@ -150,67 +103,14 @@ namespace CalcutorTest.Calculator
         }
 
         /// <summary>
-        /// Test the AcceptEquals method when there is no operator defined
+        /// Test the AcceptEquals method
         /// </summary>
         [TestMethod]
-        [ExpectedException(typeof(CalculatorException))]
         public void CalculatorServerTestEqualsNullOperator()
         {
             target.AcceptEquals();
-        }
 
-        /// <summary>
-        /// Successful test of AcceptEquals given no previous inputs
-        /// </summary>
-        [TestMethod]
-        public void CalculatorServerTestEqualsGood()
-        {
-            po.SetFieldOrProperty("currentOperator", mockOp.Object);
-            po.SetFieldOrProperty("mDisplayedNumber", 2);
-            double[] arry = new double[]{2.0};
-            mockOp.Setup(f => f.PerformOperation(arry)).Returns(3.0);
-
-            target.AcceptEquals();
-
-            double actual = (double)po.GetFieldOrProperty("mDisplayedNumber");
-            Assert.AreEqual(3.0, actual);
-            Assert.IsNull((INaryOperator)po.GetFieldOrProperty("currentOperator"));
-            Assert.AreEqual(0, previouslyEnteredNumbers.Count);
-        }
-
-        /// <summary>
-        /// Successful test of AcceptEquals given previous inputs
-        /// </summary>
-        [TestMethod]
-        public void CalculatorServerTestEqualsGoodWithInputs()
-        {
-            previouslyEnteredNumbers.Add(1.0);
-            po.SetFieldOrProperty("currentOperator", mockOp.Object);
-            po.SetFieldOrProperty("mDisplayedNumber", 2);
-            mockOp.Setup(f => f.PerformOperation(It.IsAny<double[]>())).Returns(3.0);
-
-            target.AcceptEquals();
-
-            double actual = (double)po.GetFieldOrProperty("mDisplayedNumber");
-            Assert.AreEqual(3.0, actual);
-            Assert.IsNull((INaryOperator)po.GetFieldOrProperty("currentOperator"));
-            Assert.AreEqual(0, previouslyEnteredNumbers.Count);
-        }
-
-        /// <summary>
-        /// Test when the user presses equals multiple times
-        /// </summary>
-        [TestMethod]
-        [ExpectedException(typeof(CalculatorException))]
-        public void CalculatorServerTestMultipleEquals()
-        {
-            previouslyEnteredNumbers.Add(1.0);
-            po.SetFieldOrProperty("currentOperator", mockOp.Object);
-            po.SetFieldOrProperty("mDisplayedNumber", 2);
-            mockOp.Setup(f => f.PerformOperation(It.IsAny<double[]>())).Returns(3.0);
-
-            target.AcceptEquals();
-            target.AcceptEquals();
+            mockState.Verify(f => f.EqualsTransition(), Times.Once());
         }
     }
 }
