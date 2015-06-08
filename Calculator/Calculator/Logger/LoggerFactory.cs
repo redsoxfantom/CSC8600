b50eada9b3905982161da2772cb0a930e74c17b2
Tom <redsoxfantom@gmail.com>
2015-06-08 18:28:32 -0400

    Began work on LoggerFactory

---
+++ Calculator/Calculator/Logger/LoggerFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator.Logger
+{
+    /// <summary>
+    /// Class that exposes methods for creating loggers
+    /// </summary>
+    public class LoggerFactory
+    {
+        /// <summary>
+        /// The logger output
+        /// </summary>
+        private static TextBox mOutputBox = null;
+
+        /// <summary>
+        /// Set the text box that this logger will print to.
+        /// This must be called first to initialize this factory
+        /// </summary>
+        /// <param name="outputBox">The text box</param>
+        public static void SetLoggerOutput(TextBox outputBox)
+        {
+            mOutputBox = outputBox;
+        }
+    }
+}
