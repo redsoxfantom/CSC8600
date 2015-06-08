e93f714b8a04881f49f55a41a4911370c8eb65e9
Tom <redsoxfantom@gmail.com>
2015-06-08 18:54:45 -0400

    Updated text box logger to autoscroll

--- Calculator/Calculator/Logger/TextBoxLogger.cs
+++ Calculator/Calculator/Logger/TextBoxLogger.cs
@@ -66,7 +66,7 @@ namespace Calculator.Logger
         /// <param name="str">String to print</param>
         private void Print(string str)
         {
-            mTextBox.Text += str + Environment.NewLine;
+            mTextBox.AppendText(str + Environment.NewLine);
         }
     }
 }
