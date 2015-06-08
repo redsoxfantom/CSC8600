49722ccab52e4089d53f8b6d826447d8b9553923
Tom <redsoxfantom@gmail.com>
2015-06-08 18:30:50 -0400

    Created interface for all loggers

---
+++ Calculator/Calculator/Logger/ILogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Logger
+{
+    /// <summary>
+    /// Interface for Loggers
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Prints a debug string
+        /// </summary>
+        /// <param name="str">The string</param>
+        void Debug(string str);
+
+        /// <summary>
+        /// Prints an info string
+        /// </summary>
+        /// <param name="str">The string</param>
+        void Info(string str);
+
+        /// <summary>
+        /// Prints an error string
+        /// </summary>
+        /// <param name="str">str</param>
+        void Error(string str);
+    }
+}
