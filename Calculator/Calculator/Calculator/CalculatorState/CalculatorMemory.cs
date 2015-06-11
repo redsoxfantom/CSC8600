b971ad65d3e5d48789e7827c182e61c669bf45f7
Tom <redsoxfantom@gmail.com>
2015-06-11 18:37:56 -0400

    Created the Memory class to store user values

---
+++ Calculator/Calculator/Calculator/CalculatorState/CalculatorMemory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Calculator.CalculatorState
+{
+    /// <summary>
+    /// Class that stores the value the user wanted to store
+    /// </summary>
+    public static class CalculatorMemory
+    {
+        /// <summary>
+        /// The stored memory value
+        /// </summary>
+        public static string StoredValue = null;
+    }
+}
