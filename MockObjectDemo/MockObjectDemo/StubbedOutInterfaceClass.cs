53edea394ce69253b31a47e87eb8733aaa280e30
Tom <redsoxfantom@gmail.com>
2015-06-20 11:09:25 -0400

    Added example Stub Object

---
+++ MockObjectDemo/MockObjectDemo/StubbedOutInterfaceClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockObjectDemo
+{
+    public class StubbedOutInterfaceClass : IInterfaceToMock
+    {
+        public int PerformExpensiveDatabaseLookup(int index)
+        {
+            return 5;
+        }
+    }
+}
