10941c261a0b0f1b51abf8c8c0c81ea3c99b9052
Tom <redsoxfantom@gmail.com>
2015-07-13 17:57:50 -0400

    First iteration of parallel for loop threading

--- C++Threading/C++Threading/C++Threading.cpp
+++ C++Threading/C++Threading/C++Threading.cpp
@@ -43,6 +43,8 @@ DWORD WINAPI ForThreadFunction(LPVOID params)
 	{
 		printf("Thread %d running iteration %d\n",threadId,i);
 	}
+
+	return 0;
 }
 
 void ParallelForLoop(int numIters)
@@ -51,9 +53,37 @@ void ParallelForLoop(int numIters)
 	LPSYSTEM_INFO sysInfo = (LPSYSTEM_INFO)HeapAlloc(GetProcessHeap(),HEAP_ZERO_MEMORY,sizeof(_SYSTEM_INFO));
 	GetSystemInfo(sysInfo);
 	DWORD numProc = sysInfo->dwNumberOfProcessors;
+
 	// Calculate how much work each processor will be responsible for
 	int range = numIters / numProc;
 	int remainder = numIters % numProc;
+	int finalRange = range + remainder;
+
 	// Info logging
-	printf("Running for loop using %d logical processors. Each processor will handle a range of %d. The final processor will handle a range of %d\n", numProc,range,remainder+range);
+	printf("Running for loop using %d logical processors. Each processor will handle a range of %d. The final processor will handle a range of %d\n", numProc,range,finalRange);
+	printf("Creating Threads...\n");
+
+	// Create an array of handles for the threads
+	HANDLE *ThreadArray = (HANDLE*)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(HANDLE)*numProc);
+	for (DWORD i = 0; i < numProc; i++)
+	{
+		// Allocate space for the thread and it's parameter
+		HANDLE currThread = *(ThreadArray + i);
+		pTData data = (pTData)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(myTData));
+		if (i == numProc - 1)
+		{
+			data->val = finalRange;
+		}
+		else
+		{
+			data->val = range;
+		}
+
+		// Create and run the thread
+		currThread = CreateThread(NULL, 0, ForThreadFunction, data, 0, NULL);
+	}
+
+	// Wait for all the child threads to complete
+	WaitForMultipleObjects(numProc, ThreadArray, TRUE, INFINITE);
+	printf("All for loop threads have completed\n");
 }
