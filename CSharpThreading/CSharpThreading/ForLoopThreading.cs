0bf6917fe0c6d84ff6e291e059e9243bd313d8ce
Tom <redsoxfantom@gmail.com>
2015-07-13 18:34:43 -0400

    Completed for loop example

---
+++ CSharpThreading/CSharpThreading/ForLoopThreading.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpThreading
+{
+    public class ForLoopThreading
+    {
+        public ForLoopThreading(int numIters)
+        {
+            Console.WriteLine("Creating child threads");
+            Parallel.For(0, numIters, new Action<int>((iters) =>
+            {
+                Console.WriteLine("Thread "+Thread.CurrentThread.ManagedThreadId+" processing iteration "+iters);
+                Thread.Sleep(10);
+            }));
+            Console.WriteLine("All child threads complete");
+        }
+    }
+}
--- CSharpThreading/CSharpThreading/Program.cs
+++ CSharpThreading/CSharpThreading/Program.cs
@@ -10,8 +10,19 @@ namespace CSharpThreading
     {
         static void Main(string[] args)
         {
-            BasicThreading b = new BasicThreading();
+            Console.WriteLine("Select an example program:");
+            Console.WriteLine("1) Basic example");
+            Console.WriteLine("2) For loop example");
+            String input = Console.ReadLine();
 
+            if (input == "1")
+            {
+                BasicThreading b = new BasicThreading();
+            }
+            if (input == "2")
+            {
+                ForLoopThreading f = new ForLoopThreading(100);
+            }
             Console.ReadLine();
         }
     }
