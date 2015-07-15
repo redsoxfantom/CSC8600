d8ac0634e67df9bb35e489bcea65e49bd1a8a836
Tom <redsoxfantom@gmail.com>
2015-07-14 20:55:11 -0400

    Began work on ProducerConsumer example

---
+++ CSharpThreading/CSharpThreading/ProducerConsumer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpThreading
+{
+    public class ProducerConsumer
+    {
+        private ConcurrentQueue<int> WorkItemQueue = new ConcurrentQueue<int>();
+        public CancellationToken CancelToken = new CancellationToken();
+
+        public void Run()
+        {
+            Task.Factory.StartNew(new Action(() => { Produce(); }), CancelToken);
+            Task.Factory.StartNew(new Action(() => { Consume(); }), CancelToken);
+        }
+
+        void Produce()
+        {
+            
+        }
+
+        void Consume()
+        {
+
+        }
+    }
+}
