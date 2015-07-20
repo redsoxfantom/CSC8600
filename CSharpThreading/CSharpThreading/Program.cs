8c5fdb374703f25233b3c51e37dd391774ad7192
Tom <redsoxfantom@gmail.com>
2015-07-20 19:33:44 -0400

    Added a SimpleAggregation example

--- CSharpThreading/CSharpThreading/Program.cs
+++ CSharpThreading/CSharpThreading/Program.cs
@@ -15,6 +15,7 @@ namespace CSharpThreading
             Console.WriteLine("1) Basic example");
             Console.WriteLine("2) For loop example");
             Console.WriteLine("3) Producer Consumer example");
+            Console.WriteLine("4) Simple Aggregator");
             String input = Console.ReadLine();
 
             if (input == "1")
@@ -31,6 +32,10 @@ namespace CSharpThreading
                 p.Run();
                 Thread.Sleep(2500);
                 p.CancelTokenSource.Cancel();
+            }
+            if(input == "4")
+            {
+                SimpleAggregation a = new SimpleAggregation();
             }
             Console.ReadLine();
         }
---
+++ CSharpThreading/CSharpThreading/SimpleAggregation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpThreading
+{
+    class SimpleAggregation
+    {
+        public int[] output;
+
+        public SimpleAggregation()
+        {
+            output = new int[20];
+            List<int> input = new List<int>();
+            for(int i = 0; i < 20; i++)
+            {
+                input.Add(i);
+            }
+
+            Parallel.For(0, 20, new Action<int>((val) =>
+            {
+                output[val] = val + 1;
+            }));
+
+            Console.WriteLine(String.Join(", ", output));
+        }
+    }
+}
