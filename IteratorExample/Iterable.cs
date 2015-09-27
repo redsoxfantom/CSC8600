a8a3819aec3108b5285d8d49a25833f230475ba8
Tom <redsoxfantom@gmail.com>
2015-09-27 11:57:52 -0400

    Created Iterable interface

---
+++ IteratorExample/Iterable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorExample
+{
+    public interface Iterable<T>
+    {
+        /// <summary>
+        /// Returns the first element in the collection
+        /// </summary>
+        /// <returns>The first element</returns>
+        T GetFirst();
+
+        /// <summary>
+        /// Moves the iteration to the next element in the collection
+        /// </summary>
+        void Advance();
+
+        /// <summary>
+        /// Returns the element in the collection the Iterator is currently pointing at
+        /// </summary>
+        /// <returns>The current element</returns>
+        T GetCurrent();
+
+        /// <summary>
+        /// Checks if the Iteration is at the end of the collection
+        /// </summary>
+        /// <returns>True if GetCurrent() returns the last element in the collection</returns>
+        bool AtEnd();
+    }
+}
