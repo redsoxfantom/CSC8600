05323374c83d78572b5d5f1477f4772666e557dd
Tom <redsoxfantom@gmail.com>
2015-09-27 12:02:16 -0400

    Created example Array implementation

--- IteratorExample/ArrayIteratorExample.cs
+++ IteratorExample/ArrayIteratorExample.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace IteratorExample
 {
-    class ArrayIteratorExample
+    public class ArrayIteratorExample<int> : Iterable<int>
     {
+        private int[] backingArray;
+
+        private int currentIndex;
+
+        public ArrayIteratorExample(int size)
+        {
+            backingArray = new int[size];
+            currentIndex = 0;
+
+            Random gen = new Random();
+            for(int i = 0; i < size; i++)
+            {
+                backingArray[i] = gen.Next(1000);
+            }
+        }
+
+        public int GetFirst()
+        {
+            return backingArray[0];
+        }
+
+        public void Advance()
+        {
+            currentIndex++;
+        }
+
+        public int GetCurrent()
+        {
+            return backingArray[currentIndex];
+        }
+
+        public bool AtEnd()
+        {
+            return (currentIndex == backingArray.Length);
+        }
     }
 }
